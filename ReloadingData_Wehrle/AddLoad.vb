da25c9457b802384b500f7054b055c6bc3092247
jay wehrle <wehrjr01@students.ipfw.edu>
2016-03-22 10:52:10 -0400

    test kitty

--- ReloadingData_Wehrle/AddLoad.vb
+++ ReloadingData_Wehrle/AddLoad.vb
@@ -2,4 +2,8 @@
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
     End Sub
+
+    Private Sub AddLoad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'hello kitty
+    End Sub
 End Class
